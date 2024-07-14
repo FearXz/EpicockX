bb331449bffbf479e8eca8558bf83d2311a88ee8
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-07-14 15:46:47 +0200

    Refactoring e miglioramenti vari
    Modifiche significative sono state apportate a vari file per migliorare la manutenibilità, la sicurezza e l'usabilità del codice. In particolare:
    
    - In `CartController.cs`, aggiunte e rimosse direttive `using` per pulizia e aggiornato il metodo `Success` per una maggiore coerenza nella convenzione di denominazione. Rimosso il metodo `Result` per un approccio più orientato alle sessioni.
    - `ResultOrderDto.cs` ha visto la rimozione delle proprietà `ProductName` e `Quantity` a favore di `ProductList`, indicando un cambiamento nel rappresentare i risultati degli ordini.
    - In `CartService.cs`, aggiunte e rimosse direttive `using`, migliorata la gestione delle risorse con l'esplicito rilascio delle transazioni, e modificato `GetResultOrder` per utilizzare `sessionId`. Inoltre, la logica di recupero dei risultati ordine è stata spostata su una stored procedure `setPagamento`.
    - Le modifiche a `Result.cshtml` e `Success.cshtml` riflettono i cambiamenti in `ResultOrderDto`, migliorando l'esperienza utente nella visualizzazione dei risultati degli ordini.

--- EpicockX/Controllers/CartController.cs
+++ EpicockX/Controllers/CartController.cs
@@ -1,7 +1,7 @@
-﻿using EpicockX.Models;
+﻿using System.Security.Claims;
+using EpicockX.Models;
 using EpicockX.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 
 namespace EpicockX.Controllers
 {
@@ -51,12 +51,6 @@ namespace EpicockX.Controllers
             return Redirect(session.Url);
         }
 
-        public IActionResult Result(int orderId, int userId)
-        {
-            var resultOrder = _cartSvc.GetResultOrder(orderId, userId);
-            return View(resultOrder);
-        }
-
         public IActionResult AddToCart(int id, string returnUrl)
         {
             List<Product> cart = _cartSvc.GetCart();
@@ -90,9 +84,9 @@ namespace EpicockX.Controllers
             return View();
         }
 
-        public IActionResult Success(string SessionId)
+        public IActionResult Success(string session_id)
         {
-            return View();
+            return View(_cartSvc.GetResultOrder(session_id));
         }
     }
 }
