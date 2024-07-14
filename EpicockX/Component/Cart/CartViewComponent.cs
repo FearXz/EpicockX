90272a4135a76612919b4787ad2e4ce3c1ca2fb8
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-07-14 11:15:34 +0200

    aggiunto component

---
+++ EpicockX/Component/Cart/CartViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using EpicockX.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpicockX.Component.Cart
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly CartService _cartService;
+
+        public CartViewComponent(CartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cart = _cartService.GetCart();
+            return View(cart); // Passa il carrello alla vista del View Component
+        }
+    }
+}
